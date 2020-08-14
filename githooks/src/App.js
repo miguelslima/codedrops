fd6b46eeaa0c0d2fd9a96240056b05dedc779e52
Miguel <miguelsousalima@hotmail.com>
2020-08-14 08:49:10 -0300

    Exemplo useEffect com geolocalizacao

--- githooks/src/App.js
+++ githooks/src/App.js
@@ -1,50 +1,24 @@
 import React, { useState, useEffect } from "react";
 
 export default function App() {
-  const [repositories, setRepositories] = useState([]);
+  const [location, setLocation] = useState({});
 
   useEffect(() => {
-    const runEffect = async () => {
-      const response = await fetch(
-        "https://api.github.com/users/miguelslima/repos"
-      );
-      const data = await response.json();
+    const watchId = navigator.geolocation.watchPosition(handlePositionReceived);
 
-      setRepositories(data);
-    };
-    runEffect();
+    return () => navigator.geolocation.clearWatch(watchId);
   }, []);
 
-  useEffect(() => {
-    const filtered = repositories.filter(repo => repo.favorite);
+  function handlePositionReceived({ coords }) {
+    const { latitude, longitude } = coords;
 
-    document.title = `Você tem ${filtered.length} favoritos`
-  }, [repositories])
-
-  // function handleAddRepository() {
-  //   setRepositories([...repositories, {
-  //     id: Math.random(), name: 'Novo-repo'
-  //   }]);
-  // }
-
-  function handleFavorite(id) {
-    const newRepositories = repositories.map((repo) => {
-      return repo.id === id ? { ...repo, favorite: !repo.favorite } : repo;
-    });
-    setRepositories(newRepositories);
+    setLocation({ latitude, longitude });
   }
 
   return (
     <>
-      <ul>
-        {repositories.map((repo) => (
-          <li key={repo.id}>
-            {repo.name}
-            {repo.favorite && <span>(Favorito)</span>}
-            <button onClick={() => handleFavorite(repo.id)}>Favoritar</button>
-          </li>
-        ))}
-      </ul>
+      Latitude: {location.latitude} <br />
+      Longitude: {location.longitude}
     </>
   );
 }

625c9fdad7b22487742b89e7cc8928611701c7d6
Miguel <miguelsousalima@hotmail.com>
2020-08-26 22:58:18 -0300

    Aplicacao em Next, TS, ESLint e Styled

--- reactsetup/src/pages/index.tsx
+++ reactsetup/src/pages/index.tsx
@@ -2,20 +2,20 @@ import React from 'react'
 import Head from 'next/head'
 
 import Logo from '../assets/rocketseat.svg'
+import { Container } from '../styles/pages/Home'
 
 const Home: React.FC = () => {
   return (
-    <div>
+    <Container>
       <Head>
         <title>Homepage</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
-        <Logo />
-        <h1>Hello World</h1>
-      </main>
-    </div>
+      <Logo />
+      <h1>ReactJS Structure</h1>
+      <p>A ReactJS + Next.js structure made by Rocketseat</p>
+    </Container>
   )
 }
 
